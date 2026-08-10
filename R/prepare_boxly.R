c03c2beacdba4de914d5fd50213c7844f7191254
LittleBeannie <yujie.zhao@merck.com>
2026-08-10 10:52:58 -0400

    Update the R files

--- R/prepare_boxly.R
+++ R/prepare_boxly.R
@@ -34,14 +34,49 @@
 #' @export
 #'
 #' @examples
-#' library(metalite)
-#'
-#' meta <- meta_boxly(
-#'   boxly_adsl,
-#'   boxly_adlb,
-#'   population_term = "apat",
-#'   observation_term = "wk12"
+#' analysis_plan <- metalite::plan(
+#'   analysis = "boxly",
+#'   population = "apat",
+#'   observation = "wk12",
+#'   parameter = "SODIUM;BILI"
 #' )
+#'
+#' meta <- metalite::meta_adam(
+#'   population = boxly_adsl,
+#'   observation = boxly_adlb
+#' ) |>
+#'   metalite::define_plan(analysis_plan) |>
+#'   metalite::define_population(
+#'     name = "apat",
+#'     group = "TRTA",
+#'     subset = SAFFL == "Y",
+#'     label = "Safety Population"
+#'   ) |>
+#'   metalite::define_observation(
+#'     name = "wk12",
+#'     group = "TRTA",
+#'     var = "PARAM",
+#'     subset = AVISITN <= 12 & !is.na(CHG),
+#'     label = "Weeks 0 to 12"
+#'   ) |>
+#'   metalite::define_parameter(
+#'     name = "SODIUM",
+#'     label = "Sodium (mmol/L)",
+#'     subset = PARAMCD == "SODIUM"
+#'   ) |>
+#'   metalite::define_parameter(
+#'     name = "BILI",
+#'     label = "Bilirubin (mg/dL)",
+#'     subset = PARAMCD == "BILI"
+#'   ) |>
+#'   metalite::define_analysis(
+#'     name = "boxly",
+#'     label = "Interactive Box Plot",
+#'     x = "AVISITN",
+#'     y = "CHG"
+#'   ) |>
+#'   metalite::meta_build()
+#'
 #' prepare_boxly(meta)
 #'
 #' @return Metadata list with plotting dataset
